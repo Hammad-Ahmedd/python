fdf0cf8f011589bdd1386a9eebaf2b746828f158
Hammad-Ahmed-2005 <clashofclancoc5000@gmail.com>
2025-11-04 22:55:29 +0500

    Object-Oriented Programming System

---
+++ Object-Oriented Programming System/simple-oops.program.py
@@ -0,0 +1,16 @@
+class student:
+    def __init__(self,name,age): # __init__ is the constructure
+        self.name=name
+        self.age=age
+    def printInput(self):
+        print(f"Name:{self.name} Age:{self.age}")
+
+student1=student("Hammad",19)
+student1.printInput()
+'''
+class roll:
+    a=10
+
+roll1= roll
+print(roll.a)    
+'''
